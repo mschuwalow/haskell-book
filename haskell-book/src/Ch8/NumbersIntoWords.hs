78d826c74cc1e9c44940a2bc1173e68f0f0b0406
Maxim Schuwalow <maxim.schuwalow@zalando.de>
2020-07-14 18:08:35 +0200

    add ch8

---
+++ haskell-book/src/Ch8/NumbersIntoWords.hs
@@ -0,0 +1,30 @@
+module Ch8.NumbersIntoWords where
+
+import Data.List (intercalate)
+
+digitToWord :: Int -> String
+digitToWord n =
+  case n of
+    1 -> "one"
+    2 -> "two"
+    3 -> "three"
+    4 -> "four"
+    5 -> "five"
+    6 -> "six"
+    7 -> "seven"
+    8 -> "eight"
+    9 -> "nine"
+    0 -> "zero"
+    _ -> undefined
+
+digits :: Int -> [Int]
+digits =
+  reverse . go . abs
+  where
+    go n
+      | n < 10 = [ n ]
+      | otherwise = mod n 10 : (go $ div n 10)
+
+wordNumber :: Int -> String
+wordNumber =
+   intercalate "-" . map digitToWord . digits
--- nix/haskell-book.nix
+++ nix/haskell-book.nix
@@ -3,8 +3,8 @@ mkDerivation {
   pname = "haskell-book";
   version = "0.0.0.1";
   src = ../haskell-book;
-  isLibrary = false;
-  isExecutable = true;
+  # isLibrary = true;
+  # isExecutable = false;
   license = stdenv.lib.licenses.asl20;
-  executableHaskellDepends = [ base ];
+  libraryHaskellDepends = [ base ];
 }
