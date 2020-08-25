6d3904ea89389b555075b222c97a6612c6c42a6b
Maxim Schuwalow <maxim.schuwalow@zalando.de>
2020-08-25 10:01:23 +0200

    wip

--- haskell-book/src/Arith3Broken.hs
+++ haskell-book/src/Arith3Broken.hs
@@ -3,6 +3,6 @@ main :: IO ()
 main = do
   print (1 + 2)
   putStrLn "10"
-  print "negate -1"
+  print (negate 1)
   print ((+) 0 blah)
-   where blah = negate 1
+  where blah = negate 1
--- haskell-book/src/sing.hs
+++ haskell-book/src/sing.hs
@@ -1,13 +1,8 @@
 module Sing where
 
-fstString :: [Char] ++ [Char]
+fstString :: [Char] -> [Char]
 fstString x = x ++ "in the rain"
 
-sndString :: [Char] -> Char
-sndString y = y ++ "over the rainbow"
-
-  Sing (x, y) =
-   if (x > y) then fstString x or sndString y
-      where
-        fstString = "Singin"
-        sndString = "somewhere"
+sndString :: [Char] -> [Char]
+sndString x = x ++ " over the rainbow"
+sing x y = if (x > y) then fstString "Singin" else sndString "somewhere"
