8c5ea7d025b273ce447af874d8b431d1966bf7f1
Maxim Schuwalow <maxim.schuwalow@zalando.de>
2020-09-04 03:13:10 +0200

    split excercises

--- haskell-book/src/Ch6/Eq.hs
+++ haskell-book/src/Ch6/Eq.hs
@@ -1,4 +1,4 @@
-module Ch6.Excercise where
+module Ch6.Eq where
 
 data TisAnInteger = TisAn Integer
 
@@ -40,30 +40,3 @@ instance (Eq a, Eq b) => Eq (EitherOr a b) where
   (==) (Hello a) (Hello b) = a == b
   (==) (Goodbye a) (Goodbye b) = a == b
   (==) _ _ = False
-
-fu0 :: Integer -> Bool
-fu0 a = a == 0
-
-fu :: TisAnInteger -> Bool
-fu (TisAn a) = a == 0
-
-bar :: TisAnInteger -> TisAnInteger -> Bool
-bar (TisAn a) (TisAn b) = a == b
-
-(#) :: a -> Integer
-(#) _ = 0
-
-(^) :: Integer -> Integer -> Bool
-a ^ b = (==) a b
-
-(##) :: TisAnInteger -> TisAnInteger -> Bool
-(TisAn a) ## (TisAn b) = a == b
-
--- recap
-data List a
-  = Nil
-  | Cons a (List a)
-
-head' :: List a -> a
-head' Nil = error "empty list"
-head' (Cons a _) = a
