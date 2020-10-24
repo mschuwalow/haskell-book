5886c3dc13f15eac0282a7d6a4379d189bf0bce9
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2020-10-24 22:12:39 +0200

    finish chapter 25

---
+++ haskell-book/src/Ch25/Compose.hs
@@ -0,0 +1,26 @@
+module Ch25.Compose where
+
+newtype Compose f g a
+  = Compose {getCompose :: f (g a)}
+  deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) =
+    Compose $ (fmap . fmap) f fga
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  (Compose fgh) <*> (Compose fga) =
+    Compose $ ((<*>) <$> fgh) <*> fga
+  pure a =
+    Compose $ (pure (pure a))
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldr h b (Compose fga) =
+    foldr (\ga b -> foldr h b ga) b fga
+
+instance
+  (Traversable f, Traversable g) =>
+  Traversable (Compose f g)
+  where
+  traverse h (Compose fga) =
+    fmap Compose . traverse (traverse h) $ fga
